a8b33fb192acf92f6dd142a24c66943fa584c027
Ashmit Khatiyan <ashmitkhatiyan575@gmail.com>
2024-09-09 16:39:45 +0530

    First commit

---
+++ frontend/src/landing_page/pricing/PricingPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Hero from "./Hero";
+import Brokerage from "./Brokerage";
+import OpenFound from "../OpenFound";
+
+function PricingPage() {
+  return (
+    <>
+      <Hero />
+      <OpenFound />
+      <Brokerage />
+    </>
+  );
+}
+
+export default PricingPage;
